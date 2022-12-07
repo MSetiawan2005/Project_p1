ee0c4f1a0e934838defe7f1432d1e07e8c8bfe34
MSetiawan2005 <90233076+MSetiawan2005@users.noreply.github.com>
2022-12-08 01:10:09 +0700

    update level desain
    menambahkan lvl desain.

---
+++ Assets/script/Cannon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject bullet;
+    float timebetween;
+    public float starttimebetween;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timebetween = starttimebetween;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timebetween <= 0)
+        {
+            Instantiate(bullet,firepoint.position,firepoint.rotation);
+            timebetween = starttimebetween;
+        }
+        else
+        {
+            timebetween -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/script/peluru.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class peluru : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    public GameObject effect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(effect, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+
+    }
+}
